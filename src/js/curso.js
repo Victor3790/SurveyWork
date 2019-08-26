dd7032b16f0a2ecebbb83cac35901b32eaa5c057
victor <victor182@msn.com>
2019-08-25 22:59:20 -0500

    Gallery error fixed

--- src/js/curso.js
+++ src/js/curso.js
@@ -5,15 +5,18 @@ $(document).ready(function () {
     $('.infoDetalle', rows[0]).toggle('slow');
   });
 
+  $('.cursoImgSlider').click(function () {
+    $('.cursoImgSlider').css('border', '0');
+    $(this).css('border', '4px solid blue');
+    var image = $(this).attr('src');
+    $('#imagenPrincipal').attr('src', image);
+  });
+
   new Swiper.default ('#sliderImgs', {
     // Optional parameters
-    slidesPerView: 3,
+    slidesPerView: 2,
     spaceBetween: 10,
     breakpoints: {
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-      },
       576: {
         slidesPerView: 1,
         spaceBetween: 10,
